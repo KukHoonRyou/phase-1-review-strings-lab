925c881a82d0a82000c5d3140023b77b2734762a
Kuk Hoon Ryou <coocoon09@gmail.com>
2024-02-18 03:45:52 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const currentUser = 'Michael Jordan';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+
 // Write your code in this file!
